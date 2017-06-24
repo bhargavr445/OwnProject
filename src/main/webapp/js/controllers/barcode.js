dcf1c65e610d82a612b2199ae51c02347f24a6e4
bhargavrguntaka <bhargavrguntaka@bhargavs-mbp.fios-router.home>
2017-06-24 01:30:02 -0400

    Added Splice function to include static drop down option('ALL')

--- src/main/webapp/js/controllers/barcode.js
+++ src/main/webapp/js/controllers/barcode.js
@@ -18,6 +18,10 @@ app.controller('barcodeController', function($scope,$rootScope,$http,$log,$locat
 	
 	BarcodeService.getDepartments(function(data){
 		$scope.departmentsList = data;
+
+		$scope.departmentsList.splice(0,0,
+				{"department":"ALL"}
+		);
 	});
 	
 	$scope.goToScan = function(){
@@ -55,6 +59,8 @@ app.controller('barcodeController', function($scope,$rootScope,$http,$log,$locat
 	};
 	BarcodeService.getRegionIdlist(function(data){
 		$scope.regionList = data;
+		
+		$scope.regionList.splice(0,0,"ALL");
 	});
 	$scope.getAllData = function(){
 		BarcodeService.getAllData(function(data){
@@ -67,6 +73,7 @@ app.controller('barcodeController', function($scope,$rootScope,$http,$log,$locat
 	//	obj.department = selectedData;
 		BarcodeService.getSelectedData($.param({deptName : selectedData}), function(data){
 			$scope.studentScanResult = data;
+			
 		})
 	}
 	$scope.goToview = function(student){
